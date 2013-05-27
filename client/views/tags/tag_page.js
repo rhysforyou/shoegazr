ab568a77d2d713aefd05236089710346dfd22b0b
Rhys Powell <rhys@rpowell.me>
2013-05-27 17:32:44 +1000

    Remove old 'n' busted tag page

--- client/views/tags/tag_page.js
+++
@@ -1,8 +0,0 @@
-Template.tagPage.helpers({
-  tagName: function() {
-    return Session.get('currentTagName')
-  },
-  bookmarks: function() {
-    return Bookmarks.find({tags: Session.get('currentTagName')})
-  }
-})
