2149ec460299e3bf3696ae3f82c64c9bb299bdec
Rhys Powell <rhys@rpowell.me>
2013-05-28 19:50:55 +1000

    Bookmarks can _actually_ be edited now

--- client/views/bookmarks/bookmark_edit.js
+++ client/views/bookmarks/bookmark_edit.js
@@ -6,3 +6,25 @@ Template.bookmarkEdit.helpers({
     return Bookmarks.findOne(Session.get('currentBookmarkId')).tags.join(' ')
   }
 })
+
+Template.bookmarkEdit.events({
+  'submit form': function(event) {
+    event.preventDefault()
+    var currentBookmarkId = Session.get('currentBookmarkId')
+
+    var bookmarkAttributes = {
+      title: $(event.target).find('[name=title]').val(),
+      url: $(event.target).find('[name=url]').val(),
+      description: $(event.target).find('[name=description]').val(),
+      tags: $(event.target).find('[name=tags]').val().split(' ')
+    }
+
+    Bookmarks.update(currentBookmarkId, {$set: bookmarkAttributes}, function(error) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        Meteor.Router.to('newestBookmarks')
+      }
+    })
+  }
+})
